dc355e219ebc7aedd093c7a768ed8f34f9d0613b
Dmytro Brazhnyk <dmitry@brazhnik.org>
2019-02-17 14:35:47 -0500

    Matrix service unit test

--- test/bridge_matrix_service_test.cpp
+++ test/bridge_matrix_service_test.cpp
@@ -5,10 +5,10 @@
 
 #include <iostream>
 
-bool compare_matrices(Eigen::MatrixXd& actual, Eigen::MatrixXd expected) {
+template <class M> bool compare(M& actual, M& expected) {
 	
 	bool approx = actual.rows() == expected.rows() && actual.cols() == expected.cols();
-	approx = approx && actual == expected;
+	approx = approx && actual.isApprox(expected, 0.00001);
 
 	if (!approx) {
 		std::cout << actual << std::endl << "doesn't equal to expected" << std::endl << expected << std::endl;
@@ -32,33 +32,27 @@ TEST(BridgeMatrixService, test_pendulum) {
 	simulation_model.set_gravity(Eigen::Vector2d(20, 30));
 
 	Junction& hard = bridge_model.add_hard_junction(20, 10);
-	Junction& soft = bridge_model.add_junction(24, 8);
+	Junction& soft = bridge_model.add_junction(24, 7);
 
 	bridge_model.add_girder(hard, soft);
 
 	std::unique_ptr<MatrixEquation> matrix_equation = BridgeMatrixService::create_bridge_equation(bridge_model, simulation_model, 100);
 
-	std::cout << matrix_equation->roots << std::endl;
-
-	Eigen::MatrixXd matrix_expected(6, 6);
-	matrix_expected << 
-	// F1X F1Y F12X F12Y dV1X  dV1Y
-		1,   0,   1,   0,   0,    0,
-		0,   1,   0,   1,   0,    0,
-		0,   0,  -2,  -4,   0,    0,
-		0,   0,   0,   0,   4,   -2,
-		100, 0,   0,   0,  -1,    0,
-		0, 100,   0,   0,   0,   -1;
+	Eigen::MatrixXd matrix_expected(3, 3);
+	matrix_expected <<
+		//   V1X       V1Y         F12
+		       1,        0,   -4.0 / 5,
+		       0,        1,    3.0 / 5,
+		-4.0 / 5,  3.0 / 5,          0;
 
 
-	Eigen::VectorXd right_side_exptected(6);
+	Eigen::VectorXd right_side_exptected(3);
 
-	right_side_exptected << 20, 30, 0, 0, 0, 0;
-
+	right_side_exptected << 20, 30, 0;
 	
-	ASSERT_TRUE(compare_matrices(matrix_equation->left, matrix_expected));
-	ASSERT_EQ(right_side_exptected, matrix_equation->right);
-	ASSERT_EQ(matrix_equation->get_junction_dv(0), Eigen::Vector2d(-2, -4));
+	ASSERT_TRUE(compare(matrix_equation->left, matrix_expected));
+	ASSERT_TRUE(compare(right_side_exptected, matrix_equation->right));
+	ASSERT_TRUE(compare(matrix_equation->get_junction_dv(0), Eigen::Vector2d(36 * 3.0 / 5 , 36 * 4.0 / 5)));
 }
 
 TEST(BridgeMatrixService, test_free_fall_junction) {
@@ -73,25 +67,22 @@ TEST(BridgeMatrixService, test_free_fall_junction) {
 
 	std::unique_ptr<MatrixEquation> matrix_equation = BridgeMatrixService::create_bridge_equation(bridge_model, simulation_model, 100);
 
-	Eigen::MatrixXd matrix_expected(8, 8);
+	Eigen::MatrixXd matrix_expected(4, 4);
 	matrix_expected <<
-		//F1X  F1Y  F2X  F2Y dV1X dV1Y dV2X dV2Y
-		    1,   0,   0,   0,   0,   0,   0,   0,
-		    0,   1,   0,   0,   0,   0,   0,   0,
-		    0,   0,   1,   0,   0,   0,   0,   0,
-		    0,   0,   0,   1,   0,   0,   0,   0,
-		  100,   0,   0,   0,  -1,   0,   0,   0,
-		    0, 100,   0,   0,   0,  -1,   0,   0,
-		    0,   0, 100,   0,   0,   0,  -1,   0,
-		    0,   0,   0, 100,   0,   0,   0,  -1;
-		
+		//dV1X dV1Y dV2X dV2Y
+		    1,   0,   0,   0,
+		    0,   1,   0,   0,
+		    0,   0,   1,   0,
+		    0,   0,   0,   1;
 
-	Eigen::VectorXd right_side_exptected(8);
+	Eigen::VectorXd right_side_exptected(4);
 
-	right_side_exptected << 20, 30, 20, 30, 0, 0, 0, 0;
+	right_side_exptected << 20, 30, 20, 30;
 
-	ASSERT_TRUE(compare_matrices(matrix_equation->left, matrix_expected));
-	ASSERT_EQ(right_side_exptected, matrix_equation->right);
+	ASSERT_TRUE(compare(matrix_equation->left, matrix_expected));
+	ASSERT_TRUE(compare(right_side_exptected, matrix_equation->right));
+	ASSERT_TRUE(compare(Eigen::Vector2d(20, 30), matrix_equation->get_junction_dv(0)));
+	ASSERT_TRUE(compare(Eigen::Vector2d(20, 30), matrix_equation->get_junction_dv(1)));
 }
 
 TEST(BridgeMatrixService, test_free_fall_girder) {
@@ -101,32 +92,29 @@ TEST(BridgeMatrixService, test_free_fall_girder) {
 	simulation_model.set_gravity(Eigen::Vector2d(20, 30));
 
 	Junction& j1 =  bridge_model.add_junction(10, 20);
-	Junction& j2 = bridge_model.add_junction(12, 24);
+	Junction& j2 = bridge_model.add_junction(13, 24);
 
 	bridge_model.add_girder(j1, j2);
 
 	std::unique_ptr<MatrixEquation> matrix_equation = BridgeMatrixService::create_bridge_equation(bridge_model, simulation_model, 100);
 
-	Eigen::MatrixXd matrix_expected(10, 10);
+	Eigen::MatrixXd matrix_expected(5, 5);
 	matrix_expected <<
-		//F1X  F1Y  F2X   F2Y  F12X  F12Y  dV1X  dV1Y  dV2X  dV2Y
-		    1,   0,   0,    0,   -1,   0,     0,    0,    0,    0,
-		    0,   1,   0,    0,    0,  -1,     0,    0,    0,    0,
-		    0,   0,   1,    0,    1,   0,     0,    0,    0,    0,
-		    0,   0,   0,    1,    0,   1,     0,    0,    0,    0,
-		    0,   0,   0,    0,    4,  -2,     0,    0,    0,    0,
-		    0,   0,   0,    0,    0,   0,     2,    4,    2,    4,
-		  100,   0,   0,    0,    0,   0,    -1,    0,    0,    0,
-		    0, 100,   0,    0,    0,   0,     0,   -1,    0,    0,
-		    0,   0, 100,    0,    0,   0,     0,    0,   -1,    0,
-		    0,   0,   0,  100,    0,   0,     0,    0,    0,   -1;
+		//  dV1X       dV1Y       dV2X       dV2Y      F12X
+		       1,         0,         0,         0,    3.0/5, 
+		       0,         1,         0,         0,    4.0/5, 
+		       0,         0,         1,         0,   -3.0/5, 
+		       0,         0,         0,         1,   -4.0/5, 
+		 3.0 / 5,   4.0 / 5,  -3.0 / 5,  -4.0 / 5,        0;
 
-	Eigen::VectorXd right_side_exptected(10);
+	Eigen::VectorXd right_side_exptected(5);
 
-	right_side_exptected << 20, 30, 20, 30, 0, 0, 0, 0, 0, 0;
+	right_side_exptected << 20, 30, 20, 30, 0;
 
-	ASSERT_TRUE(compare_matrices(matrix_equation->left, matrix_expected));
-	ASSERT_EQ(right_side_exptected, matrix_equation->right);
+	ASSERT_TRUE(compare(matrix_equation->left, matrix_expected));
+	ASSERT_TRUE(compare(right_side_exptected, matrix_equation->right));
+	ASSERT_TRUE(compare(Eigen::Vector2d(20, 30), matrix_equation->get_junction_dv(0)));
+	ASSERT_TRUE(compare(Eigen::Vector2d(20, 30), matrix_equation->get_junction_dv(1)));
 }
 
 TEST(BridgeMatrixService, test_hard_junction) {
@@ -145,8 +133,8 @@ TEST(BridgeMatrixService, test_hard_junction) {
 	Eigen::MatrixXd matrix_expected(0, 0);
 	Eigen::VectorXd right_side_exptected;
 
-	ASSERT_TRUE(compare_matrices(matrix_equation->left, matrix_expected));
-	ASSERT_EQ(right_side_exptected, matrix_equation->right);
+	ASSERT_TRUE(compare(matrix_equation->left, matrix_expected));
+	ASSERT_TRUE(compare(right_side_exptected, matrix_equation->right));
 }
 
 TEST(BridgeMatrixService, test_triangle) {
@@ -156,8 +144,8 @@ TEST(BridgeMatrixService, test_triangle) {
 	simulation_model.set_gravity(Eigen::Vector2d(40, 50));
 
 	Junction& j1 = bridge_model.add_hard_junction(10, 20);
-	Junction& j2 = bridge_model.add_junction(12, 18);
-	Junction& j3 = bridge_model.add_junction(7, 17);
+	Junction& j2 = bridge_model.add_junction(12, 16);
+	Junction& j3 = bridge_model.add_junction(6, 17);
 
 	bridge_model.add_girder(j1, j2);
 	bridge_model.add_girder(j2, j3);
@@ -165,29 +153,30 @@ TEST(BridgeMatrixService, test_triangle) {
 
 	std::unique_ptr<MatrixEquation> matrix_equation = BridgeMatrixService::create_bridge_equation(bridge_model, simulation_model, 15);
 
-	Eigen::MatrixXd matrix_expected(14, 14);
+	Eigen::Vector2d r23 = Eigen::Vector2d(6 - 12, 17 - 16);
+	r23.normalize();
+	
+	Eigen::MatrixXd matrix_expected(7, 7);
 	matrix_expected <<
-		//F2X  F2Y  F3X  F3Y  F12X  F12Y  F23X  F23Y  F31X  F31Y  dV2X  dV2Y  dV3X  dV3Y
-		    1,   0,   0,   0,    1,    0,  -1,    0,    0,    0,    0,    0,   0,     0,
-		    0,   1,   0,   0,    0,    1,   0,   -1,    0,    0,    0,    0,   0,     0,
-		    0,   0,   1,   0,    0,    0,   1,    0,   -1,    0,    0,    0,   0,     0,
-		    0,   0,   0,   1,    0,    0,   0,    1,    0,   -1,    0,    0,   0,     0,
-		    0,   0,   0,   0,   -2,   -2,   0,    0,    0,    0,    0,    0,   0,     0,
-		    0,   0,   0,   0,    0,    0,  -1,    5,    0,    0,    0,    0,   0,     0,
-		    0,   0,   0,   0,    0,    0,   0,    0,    3,   -3,    0,    0,   0,     0,
-		    0,   0,   0,   0,    0,    0,   0,    0,    0,    0,    2,   -2,   0,     0,
-		    0,   0,   0,   0,    0,    0,   0,    0,    0,    0,   -5,   -1,  -5,    -1,
-		    0,   0,   0,   0,    0,    0,   0,    0,    0,    0,    0,    0,   3,     3,
-		   15,   0,   0,   0,    0,    0,   0,    0,    0,    0,   -1,    0,   0,     0,
-		    0,  15,   0,   0,    0,    0,   0,    0,    0,    0,    0,   -1,   0,     0,
-		    0,   0,  15,   0,    0,    0,   0,    0,    0,    0,    0,    0,  -1,     0,
-		    0,   0,   0,  15,    0,    0,   0,    0,    0,    0,    0,    0,   0,    -1;
+	//         dV2X               dV2Y      dV3X      dV3Y              F12       F23       F31
+		          1,                 0,        0,        0,   -2.0/sqrt(20),   r23(0),        0,
+		          0,                 1,        0,        0,    4.0/sqrt(20),   r23(1),        0,
+		          0,                 0,        1,        0,               0,  -r23(0),    4.0/5,
+		          0,                 0,        0,        1,               0,  -r23(1),    3.0/5,
+     -2.0 / sqrt(20),   4.0 / sqrt(20),        0,        0,               0,        0,        0,
+		      r23(0),           r23(1),  -r23(0),  -r23(1),               0,        0,        0,
+		          0,                 0,    4.0/5,    3.0/5,               0,        0,        0;
 
 
-	Eigen::VectorXd right_side_exptected(14);
+	Eigen::VectorXd right_side_exptected(7);
 
-	right_side_exptected << 40, 50, 40, 50, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0;
+	right_side_exptected << 40, 50, 40, 50, 0, 0, 0;
 
-	ASSERT_TRUE(compare_matrices(matrix_equation->left, matrix_expected));
-	ASSERT_EQ(right_side_exptected, matrix_equation->right);
+	Eigen::Vector2d expected_dv2 = 4 * Eigen::Vector2d(4,  2);
+	Eigen::Vector2d expected_dv3 = 4 * Eigen::Vector2d(3, -4);
+
+	ASSERT_TRUE(compare(matrix_equation->left, matrix_expected));
+	ASSERT_TRUE(compare(right_side_exptected, matrix_equation->right));
+	ASSERT_TRUE(compare(expected_dv2, matrix_equation->get_junction_dv(0)));
+	ASSERT_TRUE(compare(expected_dv3, matrix_equation->get_junction_dv(1)));
 }
