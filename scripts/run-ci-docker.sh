9df794e0cbe90a9b4312c08d5910a2aea0f97e3b
Dmytro Brazhnyk <amid.ukr@gmail.com>
2019-05-25 01:36:06 -0400

    Run unit test on CI

--- scripts/run-ci-docker.sh
+++ scripts/run-ci-docker.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-cd ~/cpp-bridge-v5
+CPP_BRIDGE_V5_SOURCE="$( cd "$(dirname "$BASH_SOURCE")/.." ; pwd -P )"
+
+echo CPP_BRIDGE_V5_SOURCE = $CPP_BRIDGE_V5_SOURCE
+
+cd $CPP_BRIDGE_V5_SOURCE
 
 mkdir build
 cd build
 
 cmake .. && cmake --build .
+
+./cpp_bridge_v5_app_executable -t
