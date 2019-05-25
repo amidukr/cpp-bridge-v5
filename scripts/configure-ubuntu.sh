4510d012c31acf7ec84d6d4fad06248d630b0419
Dmytro Brazhnyk <amid.ukr@gmail.com>
2019-05-25 14:10:59 -0400

    Updating Travis CI script

--- scripts/configure-ubuntu.sh
+++ scripts/configure-ubuntu.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-sudo apt install --show-progress cmake libgtest-dev libgl-dev libglfw3-dev libglew-dev libeigen3-dev libopencv-dev
+sudo apt update
+sudo apt install --show-progress -y cmake build-essential libgtest-dev libgl-dev libglfw3-dev libglew-dev libeigen3-dev libopencv-dev git
+
+mkdir googletest
+cd googletest
+
+git clone https://github.com/google/googletest.git
+cd googletest
+mkdir gbuild && cd gbuild && sudo cmake .. && sudo make install
 
