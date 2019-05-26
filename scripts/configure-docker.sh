b01e103003200ddb659a5e7e4b0eb271ecebfa59
Dmytro Brazhnyk <amid.ukr@gmail.com>
2019-05-25 23:07:34 -0400

    Genereting test coverage html report

--- scripts/configure-docker.sh
+++ scripts/configure-docker.sh
@@ -4,5 +4,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt update
 apt upgrade -y
-apt install -yy --show-progress cmake libgtest-dev libgl-dev libglfw3-dev libglew-dev libeigen3-dev libopencv-dev
+apt install -yy --show-progress cmake libgtest-dev libgl-dev libglfw3-dev libglew-dev libeigen3-dev libopencv-dev lcov wget
+
+wget https://raw.githubusercontent.com/Taywee/args/master/args.hxx -O /usr/include/args.hxx
 
